c53d1ee50ddf67871d773a1f9ffe0a04138d2344
TiaMeds 4 <shreeraksha@tiameds.com>
2024-12-10 16:52:51 +0530

    Added token validation for getting Items

--- src/main/java/com/pharma/controller/ItemController.java
+++ src/main/java/com/pharma/controller/ItemController.java
@@ -3,18 +3,26 @@ package com.pharma.controller;
 
 import com.pharma.dto.ItemDto;
 
+import com.pharma.entity.User;
 import com.pharma.service.ItemService;
+import com.pharma.utils.ApiResponseHelper;
+import com.pharma.utils.UserAuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @CrossOrigin
 @AllArgsConstructor
 @RestController
 @RequestMapping("/pharma/item")
 public class ItemController {
+
+    private UserAuthService userAuthService;
+
 
     private ItemService itemService;
 
@@ -31,9 +39,21 @@ public class ItemController {
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<ItemDto>> getAllItem(){
+    public ResponseEntity<Map<String, Object>> getAllItem(@RequestHeader("Authorization") String token){
+
+        // Validate token format
+        Optional<User> currentUserOptional = userAuthService.authenticateUser(token);
+
+        // If user is not found, return unauthorized response
+        if (currentUserOptional.isEmpty()) {
+            return ApiResponseHelper.successResponseWithDataAndMessage("User not found", HttpStatus.UNAUTHORIZED, null);
+        }
+
+        System.out.println("currentUserOptional--------"+currentUserOptional);
+
+
         List<ItemDto> itemDtos = itemService.getAllItem();
-        return ResponseEntity.ok(itemDtos);
+        return ApiResponseHelper.successResponseWithDataAndMessage("Data Found", HttpStatus.OK,itemDtos);
     }
 
     @PutMapping("/update/{id}")
