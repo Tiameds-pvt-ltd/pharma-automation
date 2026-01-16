e3003ee2e86a312eb7ab62ce4332229accf6a7db
Abhishek Ritu <abhishekritu810219@gmail.com>
2026-01-16 15:06:53 +0530

    fix /health-check

--- src/main/java/com/pharma/config/SpringSecurityConfig.java
+++ src/main/java/com/pharma/config/SpringSecurityConfig.java
@@ -1,3 +1,102 @@
+//package com.pharma.config;
+//
+//import com.pharma.filter.JwtFilter;
+//import com.pharma.service.auth.UserDetailsServiceImpl;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity(prePostEnabled = true)
+//public class SpringSecurityConfig {
+//
+//    @Autowired
+//    private CustomAccessDeniedHandler customAccessDeniedHandler;
+//
+//    private final UserDetailsServiceImpl userDetailsService;
+//    private final JwtFilter jwtFilter;
+//
+//    public SpringSecurityConfig(UserDetailsServiceImpl userDetailsService, JwtFilter jwtFilter) {
+//        this.userDetailsService = userDetailsService;
+//        this.jwtFilter = jwtFilter;
+//    }
+//
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());        // Set the password encoder
+//        return authProvider;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//       http
+//                .csrf(csrf -> csrf.disable())  // Disable CSRF for stateless JWT authentication
+//                .cors(cors -> cors.configurationSource(new CorsConfig().corsConfigurationSource()))// Enable CORS with a custom source
+//                .authorizeHttpRequests(auth -> auth
+////                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // Permit CORS preflight request
+//                        .requestMatchers("/error").permitAll()          // Allow error endpoint without authentication
+//                        .requestMatchers("/login/**", "/register/**","/api/v1/public/health-check").permitAll()  // Public login & registration
+//                        .requestMatchers(
+//                                "/v3/api-docs/**",
+//                                "/doc/**",
+//                                "/swagger-ui/**",
+//                                "/swagger-ui.html",
+//                                "/public/**",
+//                                "/pharma/inventory/**",
+//                                "/pharma/stock/getByItemId/**",
+//                                "/pharma/variant/**",
+//                                "/test-mail",
+//                                "/auth/**"
+//                        ).permitAll()  // Allow Swagger and public resources
+//                        .anyRequest().authenticated()  // All other requests must be authenticated
+//                )
+//               .exceptionHandling(ex -> ex
+//                       .accessDeniedHandler(customAccessDeniedHandler)
+//               )
+//               .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Stateless (JWT) sessions
+//               .authenticationProvider(authenticationProvider())
+//               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);  // Add JWT filter
+//        return http.build();
+//    }
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
+//
+//
+//}
+//
+//
+//
+//
+
+
+
 package com.pharma.config;
 
 import com.pharma.filter.JwtFilter;
@@ -45,34 +144,34 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http
+        http
                 .csrf(csrf -> csrf.disable())  // Disable CSRF for stateless JWT authentication
                 .cors(cors -> cors.configurationSource(new CorsConfig().corsConfigurationSource()))// Enable CORS with a custom source
                 .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // Permit CORS preflight request
-                        .requestMatchers("/error").permitAll()          // Allow error endpoint without authentication
-                        .requestMatchers("/login/**", "/register/**","/api/v1/public/health-check").permitAll()  // Public login & registration
-                        .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/doc/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/public/**",
-                                "/pharma/inventory/**",
-                                "/pharma/stock/getByItemId/**",
-                                "/pharma/variant/**",
-                                "/test-mail",
-                                "/auth/**"
-                        ).permitAll()  // Allow Swagger and public resources
-                        .anyRequest().authenticated()  // All other requests must be authenticated
+                                .requestMatchers("/error").permitAll()          // Allow error endpoint without authentication
+                                .requestMatchers("/login/**", "/register/**","/api/v1/health-check").permitAll()  // Public login & registration
+                                .requestMatchers(
+                                        "/v3/api-docs/**",
+                                        "/doc/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/public/**",
+                                        "/pharma/inventory/**",
+                                        "/pharma/stock/getByItemId/**",
+                                        "/pharma/variant/**",
+                                        "/test-mail",
+                                        "/auth/**"
+                                ).permitAll()  // Allow Swagger and public resources
+                                .anyRequest().authenticated()  // All other requests must be authenticated
                 )
-               .exceptionHandling(ex -> ex
-                       .accessDeniedHandler(customAccessDeniedHandler)
-               )
-               .sessionManagement(session -> session
+                .exceptionHandling(ex -> ex
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                )
+                .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Stateless (JWT) sessions
-               .authenticationProvider(authenticationProvider())
-               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);  // Add JWT filter
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);  // Add JWT filter
         return http.build();
     }
 
@@ -90,6 +189,7 @@ public class SpringSecurityConfig {
 
 
 }
+
 
 
 
